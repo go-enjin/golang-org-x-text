c71d7e9664f9f82d2f74d69614574299de640c86
Kevin C. Krinke <kevin@krinke.ca>
2023-09-03 15:43:27 -0400

    [go-enjin] message/catalog (dict): refactor lookup(tag,key) to use other catalogs first

--- message/catalog/dict.go
+++ message/catalog/dict.go
@@ -34,7 +34,13 @@ func (d *dict) Lookup(key string) (data string, ok bool) {
 }
 
 func (b *Builder) lookup(tag language.Tag, key string) (data string, ok bool) {
-	return b.index.lookup(tag, key)
+	for _, c := range b.catalogs {
+		if data, ok = c.lookup(tag, key); ok {
+			return
+		}
+	}
+	data, ok = b.index.lookup(tag, key)
+	return
 }
 
 func (c *Builder) set(tag language.Tag, key string, s *store, msg ...Message) error {
